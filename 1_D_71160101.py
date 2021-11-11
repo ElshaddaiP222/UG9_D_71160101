501bf64f47762199084db1de9b05d5e77229b302
Elshaddai <elshaddai.excel@ti.ukdw.ac.id>
2021-11-11 18:34:20 +0700

    71160101_Tambah Soal 1

---
+++ 1_D_71160101.py
@@ -0,0 +1,7 @@
+u1 = int(input("uang saku anak pertama : "))
+u2 = int(input("uang saku anak kedua : "))
+u3 = int(input("uang saku anak ketiga : "))
+
+rsaku = 1/3 * (u1+u2+u3)
+
+print ("Rata-rata dari", u1,"+",u2,"+",u3, rsaku)

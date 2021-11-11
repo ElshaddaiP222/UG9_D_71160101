4f7ae13e8535da3be7c18be364ff207ece834a61
Elshaddai <elshaddai.excel@ti.ukdw.ac.id>
2021-11-11 18:34:58 +0700

    71160101_Tambah Soal 3

---
+++ 3_D_71160101.py
@@ -0,0 +1,7 @@
+b1 = str(input("Masukan daftar belanja 1 : "))
+b2 = str(input("Masukan daftar belanja 2 : "))
+b3 = str(input("Tambahkan daftar belanja 1 : "))
+b4 = str(input("Tambahkan daftar belanja 2 : "))
+
+print("Daftar belanja 1 adalah ", b1,",",b3)
+print("Daftar belanja 2 adalah ", b2,",",b4)
